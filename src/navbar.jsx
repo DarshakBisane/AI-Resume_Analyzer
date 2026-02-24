feaa7aa869a7c5e31f2c1f57a23d0fb172e93b5b
Darshak Bisane <darshak123321@gmail.com>
2026-02-24 15:04:52 +0530

    Navbar Added with complete setup

---
+++ src/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPiedPiperAlt } from "@fortawesome/free-brands-svg-icons";
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+        <div className='logo'>
+            <h1>Learn Buddy</h1>
+        </div>
+        <div className='pages'>
+            <ul className='list'>
+                <li className='listitem'>Home</li>
+                <li className='listitem'>About Us</li>
+                <li className='listitem'>Contact</li>
+                <li className='listitem'>FeedBack</li>
+                <li><button><FontAwesomeIcon icon={faPiedPiperAlt} /> Get Started</button></li>
+            </ul>
+        </div>
+    </nav>
+  )
+}
+
